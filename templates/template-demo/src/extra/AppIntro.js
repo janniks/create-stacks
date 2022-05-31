0bb28a27f07f36560be5bceef8adccebf18e26d4
janniks <janniks@users.noreply.github.com>
2022-05-31 15:53:55 +0200

    Add initial demo

---
+++ templates/template-demo/src/extra/AppIntro.js
@@ -0,0 +1,37 @@
+const AppIntro = () => {
+  return (
+    <div className="frame">
+      <h2>Stacks.js Demo 👋</h2>
+      <p>
+        This starter walks-through the basic functionality provided by{" "}
+        <code>Stacks.js</code>
+      </p>
+      <p>
+        Start by editing the components in <code>src/tabs/</code>
+      </p>
+      <span>
+        <a className="AppIntro-link" href="https://docs.hiro.so/" target="_blank">
+          Hiro Docs
+        </a>
+        <a className="AppIntro-link" href="https://stacks.js.org/" target="_blank">
+          Stacks.js Reference
+        </a>
+        <p>
+          Some features require tokens to execute. All the features shown here
+          are configured to use the <code>testnet</code>. You can get free
+          testnet-STX tokens from the{" "}
+          <a
+            href="https://explorer.stacks.co/sandbox/faucet?chain=testnet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Faucet in the Stacks Explorer
+          </a>
+          .
+        </p>
+      </span>
+    </div>
+  );
+};
+
+export default AppIntro;
